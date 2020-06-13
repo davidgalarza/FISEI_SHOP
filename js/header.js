5d2cfe1911de3f5494ac07df2b28c4a78fc64e19
David <Galarza>
2020-06-13 00:56:54 -0500

    Proteger rutas

--- js/header.js
+++ js/header.js
@@ -1,5 +1,10 @@
 autenticacion.onAuthStateChanged(function(user) {
+    if(user == null){
+        window.location.replace("./loggin.html");
+        return;
+    }
     let uid = user.uid;
+    
     base.collection('carritos').doc(uid).onSnapshot(function(doc) {
         console.log('DOC', doc)
         let cantidadProductos = 0;
