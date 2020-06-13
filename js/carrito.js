7a3e11ee25f5305845e3e7348bb4c362c4b6b511
David <Galarza>
2020-06-12 23:19:41 -0500

    Parte Michelle

--- js/carrito.js
+++ js/carrito.js
@@ -12,30 +12,120 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 let autenticacion = firebase.auth();
-let base = firebase.firestore();
+var base = firebase.firestore();
+var carrito;
 
-autenticacion.onAuthStateChanged(function(user) {
-    console.log(user);
 
-    base.collection('carritos').doc(user.uid).get().then((inf) =>{
-        if(inf.exist){
-            base.collection('carritos').doc(user.uid).update({});
-        } else{
-            base.collection('carritos').doc(user.uid).set({});
+var idUsuario;
+function idUsuarioActual(){
+    return new Promise(async (resolve, reject) =>{
+      autenticacion.onAuthStateChanged(function(user) {
+        resolve(user.uid);
+      });
+    });
+  }
+
+///////////////////////////////////////
+var totalC;
+async function actualizar(idProducto){
+    totalC=0;
+    let cant = document.getElementById(idProducto).value;
+    carrito.productos.forEach(function(e){
+        if(e.id==idProducto){
+            e.cantidad=cant;
+            e.subtotal=cant*e.precio;
         }
+        totalC=totalC+e.subtotal;      
     });
+    carrito.total=totalC;
+    await base.collection('carritos').doc(idUsuario).update(carrito);
+    principal();
+   
 
+ }
+ 
+ async function eliminar(idProducto){
+    totalC=0;
+    
+     for (let i = 0; i < carrito.productos.length; i++) {
+        if(carrito.productos[i].id==idProducto){
+            carrito.productos.splice(i,1);
+        }
+     }
+     console.log(carrito);
+     for (let i = 0; i < carrito.productos.length; i++) {
+        totalC+= carrito.productos[i].subtotal;  
+     }
+     console.log(totalC); 
+     carrito.total=totalC; 
+     await base.collection('carritos').doc(idUsuario).update(carrito);  
+     principal(); 
+ }
 
-    base.collection('carritos').doc(user.uid).get().then((inf) =>{
-        inf.productos
-    })
+///////////////////////////////////////
 
-    base.collection('carritos').doc(user.uid).update({
-        productos: [
-           {
-               cantida: 0,
-               foto: 'gghj'
-           } 
-        ]
+async function principal(){
+idUsuario= await idUsuarioActual();
+
+console.log(idUsuario);
+var respuesta = await base.collection('carritos').doc(idUsuario).get(); 
+carrito = respuesta.data();
+console.log(carrito);
+
+var request =  new XMLHttpRequest();
+var html="";
+var html2="";
+var cantP=carrito.productos.length;
+console.log(cantP);
+console.log(carrito.productos)
+if(cantP>0){
+    let contB=0;
+    carrito.productos.forEach(producto =>{
+        html += `<div class="card mb-3" style="max-width: 100%;">
+        <div class="row no-gutters">
+          <div class="col-md-2">
+            <img src="${producto.foto} " class="card-imagen" >
+          </div>
+          <div class="col-md-7">
+            <div class="card-body">
+              <h6 class="card-title">${producto.nombre} </h6>
+              <p class="card-text"><span class="etiqueta">Precio:</span> <span class="descripcion"> ${producto.precio}</span> </p>
+              <p class="card-text"> <span class="estiloPrecio">$ ${producto.subtotal} </span> </p>
+            </div>
+          </div>
+          <div  class="col-md-3">
+            <p>Cantidad</p>
+            <input class="form-control" id="${producto.id}" type="number" min=1 value="${producto.cantidad}">
+            <button type="button" id="botonAc${contB}" onclick="actualizar('${producto.id}')" class="btn btn-outline-dark"> <i class="fas fa-sync-alt"></i> Actualizar</button>
+            <button type="button" id="botonElim${contB}" onclick="eliminar('${producto.id}')" class="btn btn-outline-danger"> <i class="far fa-trash-alt"> </i> Eliminar</button>
+          </div>
+        </div>
+      </div>`
+      contB++;
+        
     });
-});
+}else{
+    html =`<div id="sinPro">
+            <div class="alert alert-primary" role="alert">
+                No tiene productos registrados!
+            </div>
+        </div>`;
+        document.getElementById('checkout').disabled=true;
+        document.getElementById('refe').hidden=true;
+}
+
+
+
+
+html2 =`<p id="precio">$ ${carrito.total} </p> `
+document.getElementById('coleccion').innerHTML=html;
+document.getElementById('coleccionSubtotal').innerHTML=html2;
+
+
+
+
+
+
+
+}
+principal();
